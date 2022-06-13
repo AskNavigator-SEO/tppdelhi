bfd12f38e43a8ac3ccb5a10dbf2b0ffa1818aa80
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-06-13 06:20:22 +0530

    test

--- test.php
+++ test.php
@@ -21,6 +21,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8DDNB8"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+<p onclick="dataLayer.push({'event': 'IamClickTestEvent'});"> Hey </p>
 
     Hello
 
