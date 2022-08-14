6eb32b7874fd02f8822d22bac304756976160234
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-08-15 00:49:31 +0530

    testcity

---
+++ include/footer.php
@@ -0,0 +1,154 @@
+
+      <!-- <=========== elaventh section ==============> -->
+      <section class="eleventh" id="cities">
+        <div class="container">
+            <h2>Cities In Which We Provide Our Service</h2>
+            <p>Selecting the best packers and movers before shifting is also a crucial decision the house owner has to take. For moving within the city, the local packers and movers are reliable. Still, for inter-city, the owner has to find out the best packer and mover as it's the question about the safety of their valuable items. The house owner should consider some tricks and tips for searching the trustworthy packers and movers in Agra.</p>
+          <div class="services-cities-list">
+            <ul>
+              <li>
+                <a href="packers-and-movers-agra.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Agra</a>
+                <a href="packers-and-movers-bangalore.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Bangalore</a>
+                <a href="packers-and-movers-bhopal.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Bhopal</a>
+                <a href="packers-and-movers-coimbatore.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Coimbatore</a>
+                <a href="/"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Delhi</a>
+                <a href="packers-and-movers-faridabad.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Faridabad</a>
+                <a href="packers-and-movers-gandhinagar.html"> <img src="/images/location.svg" alt="Location" loading="lazy"> Packers and movers in Gandhinagar</a>
+                <a href="packers-and-movers-guwahati.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Guwahati</a>
+              </li>
+              <li>
+                <a href="packers-and-movers-indore.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Indore</a>
+                <a href="packers-and-movers-jabalpur.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Jabalpur</a>
+                <a href="packers-and-movers-jamnagar.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Jamnagar</a>
+                <a href="packers-and-movers-lucknow.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Luckmow</a>
+                <a href="packers-and-movers-ludhiana.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Ludhiana</a>
+                <a href="packers-and-movers-miyapur.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Miyapur</a>
+                <a href="packers-and-movers-nagpur.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Nagpur</a>
+                <a href="packers-and-movers-nashik.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Nashik</a>
+              </li>
+              <li>
+                <a href="packers-and-movers-noida-sector62.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Noida Sector 62</a>
+                <a href="packers-and-movers-patna.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Patna</a>
+                <a href="packers-and-movers-pune.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Pune</a>
+                <a href="packers-and-movers-raipur.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Raipur</a>
+                <a href="packers-and-movers-rajkot.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Rajkot</a>
+                <a href="packers-and-movers-surat.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Surat</a>
+                <a href="packers-and-movers-thane.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Thane</a>
+                <a href="packers-and-movers-vijaywada.html"> <img src="images/location.svg" alt="Location" loading="lazy"> Packers and movers in Vijaywada</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+     
+      <!-- <=============== footer================> -->
+      <footer>
+        <div class="lower-call-btn">
+            <h3>Now get Hassle free Shifting in Lowest Cost</h3>
+            <a href="tel:+919817326797">Call Now</a>
+            </div>
+        <section class="twelth">
+
+            <div class="footer">
+                <div class="logo">
+                    <a href="#"><img src="images/logo.png" width="75px" alt="logo" loading="lazy"> <span>Thepackersandmoversdelhi</span></a>
+                </div>
+                <div class="footers-items">
+                    <div class="footer-item">
+                        <div class="footer-link-list">
+                            <h3>Product</h3>
+                            <ul>
+                                <li><a href="https://thepackersmoversdelhi.com/#home-shift">Home Shifting</a></li>
+                                <li><a href="https://thepackersmoversdelhi.com/#office-shift">Office Shifting</a></li>
+                                <li><a href="https://thepackersmoversdelhi.com/#vehicle-shift">Vehicle Shifting</a></li>
+                                <li><a href="car-transportation.html">Car Transport</a></li>
+                                <li><a href="bike-transportation.html">Bike Transport</a></li>
+                                <li> <a href="iba-approved-packers-and-movers.html">IBA Approved</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-link-list">
+                            <h3>Use Cases</h3>
+                            <ul>
+                                <li><a href="#work">How to Work</a></li>
+                                <li><a href="#faq">FAQs</a></li>
+                                <li><a href="packers-and-movers-charges.html">Cost Calculator </a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-link-list">
+                            <h3>Resources</h3>
+                            <ul>
+                                <!-- <li><a href="contact-us.html">Complaint Box</a></li> -->
+                                <li><a href="packers-and-movers-charges.html">Tools</a></li>
+                                <li><a href="contact-us.html">Customer Reviews</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-link-list">
+                            <h3>Company</h3>
+                            <ul>
+                                <li><a href="about-us.html">About Company</a></li>
+                                <li><a href="location.html">Locations</a></li>
+                                <!-- <li><a href="contact-us.html">Help</a></li> -->
+                                <li><a href="contact-us.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-link-list">
+                            <h3>Contact on social media</h3>
+                            <a href="https://m.facebook.com/103473239094318/"><img src="images/FB_icone.svg" alt="Facbook" loading="lazy"></a>
+                            <a href="https://t.me/thepackersmoversdelhi"><img src="images/TG_icone.png" alt="Telegram" loading="lazy"></a>
+                            <a href="https://instagram.com/thepackersmoversdelhi?igshid=YmMyMTA2M2Y="><img src="images/IG_icone.png" alt="Instagram" loading="lazy"></a>
+                            <form action="mail.php" method="post">
+                              <input type="number" name="PhoneNumber" id="phonenumber" placeholder="Enter Mobil No." pattern="[0-9]{10}" required>
+                              <input type="submit" id="submit" value="Get Quotes">
+                              <label for="submit"><img src="images/send_btn.svg" alt="" loading="lazy"></label>
+                            </form>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="all-Right">
+              <div class="right-link">
+                <!-- <a href="#">Privacy plicy</a> -->
+                <!-- <a href="#">Teram of Use</a> -->
+                <a href="location.html">Site Map</a>
+              </div>
+              <p> © 2022 All Rights Reserved</p>
+            </div>
+            
+        </section>
+
+
+      </footer>
+    </main>
+
+    <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "ThePackersMoversDelhi",
+        "alternateName": "TPMD",
+        "url": "https://thepackersmoversdelhi.com",
+        "logo": "https://thepackersmoversdelhi.com/images/logo.png",
+        "contactPoint": {
+          "@type": "ContactPoint",
+          "telephone": "9817326797",
+          "contactType": "customer service",
+          "contactOption": "TollFree",
+          "areaServed": "IN",
+          "availableLanguage": ["en","Hindi"]
+        },
+        "sameAs": [
+          "https://thepackersmoversdelhi.com",
+          "https://m.facebook.com/103473239094318/",
+          "https://instagram.com/thepackersmoversdelhi?igshid=YmMyMTA2M2Y="
+        ]
+      }
+      </script>
+
+  </body>
+
+  <script src="js/script.js"></script>
+</html>
---
+++ include/header.php
@@ -0,0 +1,50 @@
+<header>
+      <!-- <======== navbar =======> -->
+      <div class="navigatipnbar">
+        <div class="nav">
+          <input type="checkbox" id="nav-check" />
+          <div class="nav-header">
+            <div class="nav-title">
+              <a href="/">
+                <img src="images/logo.png" width="40px" alt="logo" loading="lazy"/>
+                <span> ThePackersmoversDelhi</span>
+              </a>
+            </div>
+          </div>
+          <div class="nav-btn">
+            <!-- <======== hemberger =======> -->
+            <label for="nav-check">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div>
+          <!-- <========  menu =======> -->
+          <div class="nav-links">
+            <a href="/">Home</a>
+            <a href="location.html">Cities</a>
+            <a href="about-us.html">About Us</a>
+            <a href="contact-us.html">Contact Us</a>
+            <a href="tel:9817326797" class="callBTN">Call Now</a>
+          </div>
+          <div class="call_btn">
+            <a href="tel:+919817326797">
+              <img src="images/head_call_btn.png" alt="call button" loading="lazy"/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- <=========== main ==========> -->
+    <main>
+      <!-- <============ first section ==========> -->
+      <section class="first">
+        <div class="hero_continar">
+          <!-- <======== back shadow =======> -->
+          <img
+            src="images/1st_black_line.svg"
+            width="100%"
+            height="880px"
+            alt="shadow" loading="lazy"
+          />
