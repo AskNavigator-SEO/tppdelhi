02d8182b22d9fd707348ba236c4cac4f899f7bdf
Devender Singh <99714664+DevendersinghRajput@users.noreply.github.com>
2022-08-18 12:16:04 +0530

    meta add

--- include/head.php
+++ include/head.php
@@ -1,8 +1,10 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="index, follow">
+    
     <link rel=icon type=image/x-icon href="images/logo.png">
     <link rel="stylesheet" href="css/style.css?v=0.0.7" />
     <link rel="stylesheet" href="css/location.css?v=0.0.7" />
     <link rel="stylesheet" href="css/about.css?v=0.0.7" />
-    <meta name=robots content="index, follow">
+    <link rel=icon type=image/x-icon href="images/logo.png">
