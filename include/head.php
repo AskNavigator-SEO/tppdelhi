8b5f53e7907e12a81cef78225c9ac2fcec91ebee
Devender Singh <99714664+DevendersinghRajput@users.noreply.github.com>
2022-08-18 11:50:51 +0530

    version change css file

--- include/head.php
+++ include/head.php
@@ -2,7 +2,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel=icon type=image/x-icon href="images/logo.png">
-    <link rel="stylesheet" href="css/style.css?v=0.0.6" />
-    <link rel="stylesheet" href="css/location.css?v=0.0.6" />
-    <link rel="stylesheet" href="css/about.css?v=0.0.6" />
+    <link rel="stylesheet" href="css/style.css?v=0.0.7" />
+    <link rel="stylesheet" href="css/location.css?v=0.0.7" />
+    <link rel="stylesheet" href="css/about.css?v=0.0.7" />
     <meta name=robots content="index, follow">
